346c46918a2be0b55e1a324b54d37d88e47707cb
Kiptanui Boaz <boaserem022@gmail.com>
2024-01-01 02:50:48 +0300

    added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
+import UserCard from "./components/UserCard"
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(options)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home
-    </main>
+    <>
+      {session ? (
+        <UserCard user={session?.user} pagetype={"Home"} />
+      ) : (
+        <h1 className="text-5xl">You Shall Not Pass!</h1>
+      )}
+    </>
   )
 }
